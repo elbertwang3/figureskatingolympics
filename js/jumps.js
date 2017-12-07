20575c7b6d576959002f8af76db5eb7c7abfea30
elbertwang3 <elbert_wang@alumni.brown.edu>
2017-12-06 17:50:26 -0800

    women done

--- js/jumps.js
+++ js/jumps.js
@@ -38,7 +38,7 @@ function ready(error,mens, womens) {
 
 	var womensYScale = d3.scaleLinear()
 		.domain([0,womensjumparray.length-1]) //gonna need to use indexof on jumparray to get this value
-		.range([jumpsMargin.top, jumpsWomenHeight-jumpsMargin.bottom])
+		.range([jumpsMargin.top, jumpsMenHeight-jumpsMargin.bottom])
 
 	var mensXScale = d3.scaleLinear()
 		.domain([0,10])
@@ -46,10 +46,11 @@ function ready(error,mens, womens) {
 
 	var womensXScale = d3.scaleLinear()
 		.domain([0,9])
-		.range([jumpsMargin.left+30, jumpsWomenWidth-jumpsMargin.right])
+		.range([jumpsMargin.left+45, jumpsMenWidth-jumpsMargin.right+15])
 
 	var goescale = d3.scaleLinear().domain([3,0,-3]).range(["#2161fa","#dddddd","#ff3333"]);
 	var bvscale = d3.scaleLinear().domain([0,13.6]).range([2,10])
+	var womensbvscale = d3.scaleLinear().domain([0,8.5]).range([2,10])
 
 
 
@@ -58,7 +59,7 @@ function ready(error,mens, womens) {
 		skatercountry = mens[i].country;
 		
 
-		currentdiv = mendiv
+		var currentdiv = mendiv
 			.append('div')
 			.attr('class', 'skater-div')
 		var numquads = 0
@@ -70,7 +71,6 @@ function ready(error,mens, womens) {
 		       	
 		    }
 		}
-		//console.log(numquads);
 		header = currentdiv.append("div")
 			.attr("class","header")
 			
@@ -119,22 +119,12 @@ function ready(error,mens, womens) {
 			.attr('width', jumpsMenWidth)
 			.attr('height', jumpsMenHeight)
 
-		
-		
 		yaxis = currentsvg.append("g")
 			.attr("class", "y-axis")
 		
 		horizontallines = currentsvg.append("g")
 			.attr("class", "horizontal-lines")
 
-		
-			/*.attr("transform", function(d,i) { 
-				if (i == 0) {
-					return "translate("+mensXScale(1.5)+",0)";
-				} else if (i == 1) {
-					return "translate("+mensXScale(6.5)+",0)";
-				}
-			})*/
 		revolutions = yaxis.append("g")
 			.attr("class", "revolutions")
 
@@ -190,6 +180,16 @@ function ready(error,mens, womens) {
 			.attr('x2', jumpsMenWidth)
 			.attr('y2', 0);
 
+		dividingline = currentsvg.append("g")
+	    	.attr("class", "dividing-line")
+	    dividingline.append("line")
+	    	.attr("class", 'x-axis-lines')
+			.attr("transform", function(d,i) { return "translate("+mensXScale(2.5) + ",0)";})
+			.attr('x1', 0)
+			.attr('y1', jumpsMargin.top)
+			.attr('x2', 0)
+			.attr('y2', jumpsMenHeight - jumpsMargin.bottom);
+	    dividingline.append("line")
 		/*skaterimages.selectAll("skater-images-img")
 			.data*/
 		skaterimages.selectAll(".skaterimages")
@@ -203,8 +203,6 @@ function ready(error,mens, womens) {
 				} else if (i == 1) {
 					return "skater-image-img fs";
 				}
-				//return "skater-image-img";
-				
 			})
 
 			.attr("src", function(d) { return d;})
@@ -285,7 +283,6 @@ function ready(error,mens, womens) {
 					for (var j = 0; j < split.length; j++) {
 						toReturn.push([split[j],d[1],d[2]])
 					}
-					console.log(toReturn);
 					return toReturn;
 				} else {
 					return [];
@@ -293,7 +290,7 @@ function ready(error,mens, womens) {
 			})
 			.enter()
 			.append("circle")
-			.attr("r", function(d) { console.log(d); return bvscale(mensbvarray[mensjumparray.indexOf(jumphelper(d[0]))]) * 1.1;})
+			.attr("r", function(d) { return bvscale(mensbvarray[mensjumparray.indexOf(jumphelper(d[0]))]) * 1.1;})
 			.attr("class", "secondhalf")
 			.attr("cy", function(d) { return mensYScale(mensjumparray.indexOf(jumphelper(d[0])))})
 			//.style("fill","transpare")
@@ -321,35 +318,7 @@ function ready(error,mens, womens) {
 			.attr("cy", function(d) { return mensYScale(mensjumparray.indexOf(jumphelper(d[0])))})
 			.style("fill", function(d) { return goescale(d[1]); })
 			.style("stroke-width", 1.5)
-
-
-
-		/*jumpsequence.selectAll(".secondhalfs")
-			.data(function(d) { 
-		
-				split = d[0].split("+")
-
-				var toReturn = []
-				for (var j = 0; j < split.length; j++) {
-					toReturn.push([split[j],d[1]])
-				}
-
-				return toReturn;
-			})
-			.enter()
-			.append("circle")
-			.attr("r", function(d) { return bvscale(mensbvarray[mensjumparray.indexOf(jumphelper(d[0]))]) * 1.1;})
-			.attr("class", "secondhalf")
-			.attr("cy", function(d) { return mensYScale(mensjumparray.indexOf(jumphelper(d[0])))})
-			.style("fill","white")
-			//.style("stroke-color", function(d) { return goescale(d[1]); })
-			.style("stroke", "black")
-			.style("stroke-width", 1.5)*/
-
-
-			
-		
-						
+								
 
 		programlabels = currentsvg.append("g")
 						.attr("class", "program-labels")
@@ -362,9 +331,9 @@ function ready(error,mens, womens) {
 			.text(function(d) { return d;})
 			.attr("x", function(d,i) {
 				if (i == 0) {
-					return mensXScale(0);
+					return mensXScale(1);
 				} else {
-					return mensXScale(3);
+					return mensXScale(6.5);
 				}
 				
 			})
@@ -372,10 +341,308 @@ function ready(error,mens, womens) {
 			.attr("font-family", "Roboto")
 	    	.attr("font-size", "12px")
 	    	.attr("text-anchor", "start")
+	    	.attr("text-anchor", "middle");
+	}
+	for (var i = 0; i < womens.length; i++) {
+		console.log(womens[i])
+		skatername = womens[i].skater;
+		skatercountry = womens[i].country;
+		
 
+		var currentdiv = womendiv
+			.append('div')
+			.attr('class', 'skater-div')
+		var numtriples = 0
+		for (var key in womens[i]) {
 
+		    if (womens[i].hasOwnProperty(key)) {
 
-	    function jumpsmouseOverEvents(data, element) {
+		    	splitjump = womens[i][key].split("+")
+		    	for (var j = 0; j < splitjump.length; j++) {
+	
+		    		if (splitjump[j].charAt(0) == "3") {
+		    			numtriples += 1
+		    		}
+		    	}
+		       	
+		    }
+		}
+		console.log(numtriples)
+		header = currentdiv.append("div")
+			.attr("class","header")
+			
+		quadsdiv = header.append("div")
+			.attr("class","quadsdiv")
+		
+		
+		quadsdiv.append('div')
+				.text(numtriples)
+				.attr("class", "numquads")
+		quadjumps = quadsdiv.append('div')
+			.attr("class", "quadjumps")
+		
+		quadjumps.append('span')
+				.text("triple")
+				.attr("class", "quad text")
+		quadjumps.append('span')
+				.text(function() {
+					if (numtriples == 1) {
+						return "jump";
+					} else {
+						return "jumps";
+					}
+				})
+				.attr("class", "jumps text")
+				
+				
+
+		namecountrydesc = header.append("div")
+			.attr("class", "namecountrydesc")
+		namecountry = namecountrydesc.append("div")
+			.text(skatername)
+			.attr("class","namecountry")
+		namecountry.append("img")
+			.attr('src', "images/"+ skatercountry+"flag.png")
+		namecountrydesc.append("div")
+			.text(mens[i].desc)
+			.attr("class","desc")
+
+		skaterimages = currentdiv.append("div")
+			.attr("class", "skater-images")
+		svgdiv = currentdiv.append('div')
+			.attr("class", 'svg-div')
+		currentsvg = svgdiv.append('svg')
+			.attr('class', 'skater-svg')
+			.attr('width', jumpsMenWidth)
+			.attr('height', jumpsMenHeight)
+
+		yaxis = currentsvg.append("g")
+			.attr("class", "y-axis")
+		
+		horizontallines = currentsvg.append("g")
+			.attr("class", "horizontal-lines")
+
+		revolutions = yaxis.append("g")
+			.attr("class", "revolutions")
+
+		jumptype = yaxis.append("g")
+			.attr("class", "jumptype")
+		jumpsequences = currentsvg.append("g")
+			.attr("class", "jump-sequences")
+
+		currentrev = ""
+
+		revolutions.selectAll('.revolution')
+			.data(womensjumparray)
+			.enter()
+			.append("text")
+			.text(function(d) {
+				if (d.split(" ")[0] != currentrev) {
+					currentrev = d.split(" ")[0];
+					return d.split(" ")[0];
+
+				} else {
+					return "";
+				}
+			})
+			.attr('x', jumpsMargin.left-5)
+	    	.attr('y', function(d) { return womensYScale(womensjumparray.indexOf(d));})
+	    	.attr("font-family", "Roboto")
+	    	.attr("font-size", "12px")
+	    	.attr("text-anchor", "end")
+	    	.attr("font-weight", 900)
+	    	.attr("alignment-baseline", "middle")
+		jumptype.selectAll('.jumptype')
+			.data(womensjumparray)
+			.enter()
+			.append("text")
+			.text(function(d) {
+				return d.split(" ")[1];
+			})
+			.attr('x', jumpsMargin.left)
+	    	.attr('y', function(d) { return womensYScale(womensjumparray.indexOf(d));})
+	    	.attr("font-family", "Roboto")
+	    	.attr("font-size", "12px")
+	    	.attr("text-anchor", "start")
+	    	.attr("alignment-baseline", "middle")
+
+	    horizontallines.selectAll(".horizontalline")
+	    	.data(mensjumparray)
+	    	.enter()
+			.append("line")
+			.attr("class", 'y-axis-lines')
+			.attr("transform", function(d,i) { return "translate(0," + womensYScale(mensjumparray.indexOf(d)) + ")";})
+			.attr('x1', jumpsMargin.left + 30)
+			.attr('y1', 0)
+			.attr('x2', jumpsMenWidth)
+			.attr('y2', 0);
+
+		dividingline = currentsvg.append("g")
+	    	.attr("class", "dividing-line")
+	    dividingline.append("line")
+	    	.attr("class", 'x-axis-lines')
+			.attr("transform", function(d,i) { return "translate("+womensXScale(2.5) + ",0)";})
+			.attr('x1', 0)
+			.attr('y1', jumpsMargin.top)
+			.attr('x2', 0)
+			.attr('y2', jumpsMenHeight - jumpsMargin.bottom);
+	    dividingline.append("line")
+		/*skaterimages.selectAll("skater-images-img")
+			.data*/
+		skaterimages.selectAll(".skaterimages")
+	
+			.data(['images/skaters/' + skatername.split(" ")[0] + "sp.png?xxx=987878787",'images/skaters/' + skatername.split(" ")[0] + "fs.png?xxx=987878787"])
+			.enter()
+			.append("img")
+			.attr("class", function(d, i) {
+				if (i == 0) {
+					return "skater-image-img sp";
+				} else if (i == 1) {
+					return "skater-image-img fs";
+				}
+			})
+
+			.attr("src", function(d) { return d;})
+			.style("top", jumpsMargin.top)
+
+				
+
+		var picked = (({spj1,spj2,spj3,fsj1,fsj2,fsj3,fsj4,fsj5,fsj6,fsj7}) => ({spj1,spj2,spj3,fsj1,fsj2,fsj3,fsj4,fsj5,fsj6,fsj7}))(womens[i])
+		var goes = (({gspj1,gspj2,gspj3,gfsj1,gfsj2,gfsj3,gfsj4,gfsj5,gfsj6,gfsj7}) => ({gspj1,gspj2,gspj3,gfsj1,gfsj2,gfsj3,gfsj4,gfsj5,gfsj6,gfsj7}))(womens[i])
+		var secondhalves = womens[i].secondhalf.split(",")
+
+		for (var key in goes) {
+		    if (goes.hasOwnProperty(key)) {
+
+		    	goes[key] = d3.mean(JSON.parse(goes[key])).toFixed(2)
+
+		    }
+		}
+
+		var result = Object.keys(picked).map(function(key) {
+		  return [picked[key],goes['g'+key]];
+		  //return picked[key];
+		});
+		for (var z = 0; z < result.length; z++) {
+			result[z].push(secondhalves[z])
+		}
+		
+		jumpsequence = jumpsequences.selectAll(".jump-sequence")
+			.data(result)
+			.enter()
+			.append("g")
+			.attr("class", "jump-sequence")
+			.attr("transform", function(d,i) { return "translate(" + womensXScale(i) + ",0)";})
+			.on('mouseover',function(d) {
+				data = d
+				jumpsmouseOverEvents(data, d3.select(this))
+				d3.select(this).classed("hover", true);
+			})
+			.on('mouseout', function(d) {
+				data = d
+				jumpsmouseOutEvents(data, d3.select(this))
+				d3.select(this).classed("hover", false);
+			})
+		jumpsequence.selectAll(".connectors")
+			.data(function(d) { 
+				var split = d[0].split("+");
+				
+				if (split.length > 1) { 
+					var toReturn = []
+					for (var j = 0; j < split.length-1; j++) {
+						
+						var combo = split.slice(j, j+2).join("+")
+						toReturn.push(combo)
+					}
+					//console.log(toReturn)
+					return toReturn;
+				} else {
+					return [];
+				}
+			})
+			.enter()
+			.append("line")
+			.attr("class", "connector")
+			.attr('x1', 0)
+			.attr('y1', function(d) { return womensYScale(womensjumparray.indexOf(jumphelper(d.split("+")[0])))})
+			.attr('x2', 0)
+			.attr('y2', function(d) { return womensYScale(womensjumparray.indexOf(jumphelper(d.split("+")[1])))})
+
+		
+		jumpsequence.selectAll(".secondhalfs")
+			.data(function(d) { 
+		
+				split = d[0].split("+")
+
+				console.log(d);
+				if (d[2] == "x") {
+					var toReturn = []
+					for (var j = 0; j < split.length; j++) {
+						toReturn.push([split[j],d[1],d[2]])
+					}
+					console.log(toReturn);
+					return toReturn;
+				} else {
+					return [];
+				}
+			})
+			.enter()
+			.append("circle")
+			.attr("r", function(d) { console.log(d); return womensbvscale(womensbvarray[womensjumparray.indexOf(jumphelper(d[0]))]) * 1.1;})
+			.attr("class", "secondhalf")
+			.attr("cy", function(d) { return womensYScale(womensjumparray.indexOf(jumphelper(d[0])))})
+			//.style("fill","transpare")
+			//.style("stroke-color", function(d) { return goescale(d[1]); })
+			.style("stroke", function(d) { return goescale(d[1]); })
+			.style("stroke-width", 1.5)
+			
+		
+		jumpsequence.selectAll(".individualjumps")
+			.data(function(d) { 
+		
+				split = d[0].split("+")
+	
+				var toReturn = []
+				for (var j = 0; j < split.length; j++) {
+					toReturn.push([split[j],d[1]])
+				}
+
+				return toReturn;
+			})
+			.enter()
+			.append("circle")
+			.attr("r", function(d) { return womensbvscale(womensbvarray[womensjumparray.indexOf(jumphelper(d[0]))]);})
+			.attr("class", "individual-jump")
+			.attr("cy", function(d) { return womensYScale(womensjumparray.indexOf(jumphelper(d[0])))})
+			.style("fill", function(d) { return goescale(d[1]); })
+			.style("stroke-width", 1.5)
+								
+
+		programlabels = currentsvg.append("g")
+						.attr("class", "program-labels")
+		labels = ["short program", "free skate"]
+		programlabels.selectAll("program-labels")
+			.data(labels)
+			.enter()
+			.append("text")
+			.attr("class", "program-labels")
+			.text(function(d) { return d;})
+			.attr("x", function(d,i) {
+				if (i == 0) {
+					return womensXScale(1);
+				} else {
+					return womensXScale(6);
+				}
+				
+			})
+			.attr("y", jumpsMargin.top - 20)
+			.attr("font-family", "Roboto")
+	    	.attr("font-size", "12px")
+	    	.attr("text-anchor", "start")
+	    	.attr("text-anchor", "middle");
+		
+	}
+	function jumpsmouseOverEvents(data, element) {
 	    
 	    	jumpstooltip
 					
@@ -400,12 +667,6 @@ function ready(error,mens, womens) {
 	    	jumpstooltip
 	       		.style("visibility",null);
 		}
-
-	}
-	for (var i = 0; i < womens.length; i++) {
-		
-	}
-	
 	function jumphelper(abbrev) {
 		var revs = abbrev.substring(0,1)
 		var jump = abbrev.substring(1)
